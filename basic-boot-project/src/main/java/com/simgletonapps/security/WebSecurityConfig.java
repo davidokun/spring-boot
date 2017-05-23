59f92b6f1996f8a13849124ab9114496e711be2e
David Cuellar Marín <david.cuellar@globant.com>
2017-05-23 11:49:02 -0500

    Activate CSRF

--- basic-boot-project/src/main/java/com/simgletonapps/security/WebSecurityConfig.java
+++ basic-boot-project/src/main/java/com/simgletonapps/security/WebSecurityConfig.java
@@ -12,8 +12,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().fullyAuthenticated().and().
-                httpBasic().and().
-                csrf().disable();
+                httpBasic();
     }
 
 }
