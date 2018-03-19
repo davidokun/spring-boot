5b49045e6a99bc72b419b5c97eb74babf602dbb3
davidokun <davidokun99@gmail.com>
2018-03-19 11:13:55 -0500

    Adding Post Controller.

---
+++ spring-boot-cache-sample/src/main/java/com/singletonapps/demospringcache/controller/PostController.java
@@ -0,0 +1,27 @@
+package com.singletonapps.demospringcache.controller;
+
+import com.singletonapps.demospringcache.model.Post;
+import com.singletonapps.demospringcache.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getPosts() {
+        return postService.getPosts();
+    }
+}
