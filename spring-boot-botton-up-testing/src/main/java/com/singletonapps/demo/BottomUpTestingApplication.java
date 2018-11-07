698dba57475bdab1b474daec0169e3d40e77cbe8
davidokun <davidokun99@gmail.com>
2018-11-07 19:43:06 +0100

    Initial project setup.

---
+++ spring-boot-botton-up-testing/src/main/java/com/singletonapps/demo/BottomUpTestingApplication.java
@@ -0,0 +1,12 @@
+package com.singletonapps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BottomUpTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BottomUpTestingApplication.class, args);
+    }
+}
