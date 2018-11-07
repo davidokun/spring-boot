d104f5c570169c6507ed6dea9be2fb6470b5559a
davidokun <davidokun99@gmail.com>
2018-11-07 20:39:26 +0100

    Game JPA mapping tests.

---
+++ spring-boot-botton-up-testing/src/main/java/com/singletonapps/demo/model/Game.java
@@ -0,0 +1,24 @@
+package com.singletonapps.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Long yearPublished;
+}
---
+++ spring-boot-botton-up-testing/src/test/java/com/singletonapps/demo/mapping/GameMappingTest.java
@@ -0,0 +1,39 @@
+package com.singletonapps.demo.mapping;
+
+import com.singletonapps.demo.model.Game;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class GameMappingTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testModelPersistenceAnnotations() {
+
+        //given
+        Game game = Game.builder()
+            .id(null)
+            .name("Mario Bros")
+            .yearPublished(1985L)
+            .build();
+
+        //when
+        Game gameSaved = entityManager.persistFlushFind(game);
+
+        //then
+        assertThat(gameSaved.getId()).isEqualTo(1L);
+        assertThat(gameSaved.getName()).isEqualTo("Mario Bros");
+        assertThat(gameSaved.getYearPublished()).isEqualTo(1985L);
+
+    }
+}
