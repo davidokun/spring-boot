163b07d1c9bdc7be4af6fcadc4353b8b1c4e08b1
davidokun <davidokun99@gmail.com>
2018-11-11 22:28:25 +0100

    Add script to clean database after integration test finish.

--- spring-boot-botton-up-testing/src/test/java/com/singletonapps/demo/GameControllerIntegrationTest.java
+++ spring-boot-botton-up-testing/src/test/java/com/singletonapps/demo/GameControllerIntegrationTest.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     classes = BottomUpTestingApplication.class
 )
 @AutoConfigureMockMvc
+@Sql(scripts = "classpath:delete.sql", executionPhase = AFTER_TEST_METHOD)
 public class GameControllerIntegrationTest {
 
     @Autowired
