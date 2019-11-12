c87ff10a8d6c15e8c7d1e21c7c3f6c3e54005f8b
davidokun <davidokun99@gmail.com>
2019-11-12 14:10:01 +0100

    Adding endpoint and limit config

---
+++ currency-exchange-app/limits-service/src/main/java/com/darksideofthedev/microservices/limitsservice/config/LimitsConfiguration.java
@@ -0,0 +1,14 @@
+package com.darksideofthedev.microservices.limitsservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "limits-service")
+public class LimitsConfiguration {
+
+    private int minimum;
+    private int maximum;
+}
