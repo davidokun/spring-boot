32b5be88712191db51f98151081e679699ed2318
davidokun <davidokun99@gmail.com>
2020-04-14 11:46:41 +0200

    Initial project set up for recipe app.

---
+++ spring-boot-recipe-app/src/main/java/com/singletonapps/springbootrecipe/SpringBootRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.singletonapps.springbootrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecipeAppApplication.class, args);
+    }
+
+}
